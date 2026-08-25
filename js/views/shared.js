99a114b0cbf6b137f1e14c736d8ae184d0657b80
João Pedro <joaopedrotorres366@gmail.com>
2026-08-25 20:41:26 -0300

    feat: Código organizado

---
+++ js/views/shared.js
@@ -0,0 +1,9 @@
+export function branchDivider() {
+  return `<div class="branch">
+    <svg viewBox="0 0 120 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 8 H45" stroke="currentColor" stroke-width="1"/>
+      <circle cx="60" cy="8" r="3" fill="currentColor"/>
+      <path d="M75 8 H120" stroke="currentColor" stroke-width="1"/>
+    </svg>
+  </div>`;
+}
